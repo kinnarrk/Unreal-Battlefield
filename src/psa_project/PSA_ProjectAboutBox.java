4e9afad3b62eb0fa05971e2d057d6bec6f71858c
Kansara Kinnar <kansara.k@husky.neu.edu>
2019-08-02 16:08:42 -0400

    About box changes in new branch

--- src/psa_project/PSA_ProjectAboutBox.java
+++ src/psa_project/PSA_ProjectAboutBox.java
@@ -98,7 +98,7 @@ public class PSA_ProjectAboutBox extends javax.swing.JDialog {
                             .addComponent(appVendorLabel)
                             .addComponent(appHomepageLabel)))
                     .addComponent(appTitleLabel, javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(appDescLabel, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 266, Short.MAX_VALUE)
+                    .addComponent(appDescLabel, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
                     .addComponent(closeButton))
                 .addContainerGap())
         );
