73ef32809f4d6f2734af17c662b6f1bf10e65b95
Bhavya Bharatbhai Patel <patel.bhavya@husky.neu.edu>
2019-08-09 14:58:13 -0400

    Cars and Stops initialised

--- src/MobileCharging/MobileCharging.java
+++ src/MobileCharging/MobileCharging.java
@@ -10,7 +10,16 @@ package MobileCharging;
  * @author bhaVYa
  */
 public class MobileCharging {
+    private static final int STOP = 40;
+    private static final int TOTAL_CARS = 5;
+    private static final int MIN_CAR_BATTERY = 15;
+    private static final int MAX_CAR_BATTERY = 50;
+    private static final int TOTAL_PARTICLES = 30;
+    private static final int TOTAL_ITERATIONS = 40;
+    
     public static void main(String[] args) {
+        MobileChargingDirectory directory = new MobileChargingDirectory(STOP, TOTAL_CARS, MIN_CAR_BATTERY, MAX_CAR_BATTERY);
+        
         
     }
 }
